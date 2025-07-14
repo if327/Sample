b39be15c9d865e4e8bb48d7b8c22059d74f8a148
堀内樹\nekoh <nekoha0805@gmail.com>
2025-07-14 18:56:09 +0900

    Enemyの挙動、オブジェクト生成

---
+++ Assets/script/EnemyMaker.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMaker : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private float makeTime;
+    private float waitTime;
+    [SerializeField] private float enemyY;
+    [SerializeField] private float enemyX;
+    private float ranX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waitTime < makeTime)
+        {
+            waitTime = waitTime + Time.deltaTime;
+        }
+
+        else
+        {
+            ranX = Random.Range(enemyX * -1, enemyX);
+
+            Instantiate(enemyPrefab, new Vector3(ranX, enemyY, 0), enemyPrefab.transform.rotation);
+
+            waitTime = 0;
+        }
+    }
+}
